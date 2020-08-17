8e498076007383c13840fe091e47fe06e9fd7d13
Andrew Yang <andrew@kali>
2020-08-16 23:59:49 -0400

    get post working

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,12 @@ connection.once("open", () => {
   console.log("Database is connected successfully to our server");
 });
 
+const exerciseRouter = require("./routes/exercises");
+const userRouter = require("./routes/users");
+
+app.use("/exercises", exerciseRouter);
+app.use("/users", userRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
